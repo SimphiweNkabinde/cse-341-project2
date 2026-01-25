10b2b04dda3b50425f505d0e2cb68c105a6f6080
SimphiweNkabinde <simphiwenkabinde.dev@gmail.com>
2026-01-25 22:47:11 +0200

    update categories routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.use("/budget-items", require("./budgetItem"));
 router.use("/transactions", require("./transaction"));
-router.use("/category", require("./category"));
+router.use("/categories", require("./category"));
 router.use("/api-docs", require("./apiDocs"));
 
 module.exports = router;
